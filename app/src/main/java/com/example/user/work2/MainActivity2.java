bbc28a31aeb23f46844ff0ce0a0974190d13bd82
user188245 <kangsg10@naver.com>
2017-03-16 17:24:12 +0900

    Work2 첫 push, 실습 시간 마친 직후
    아직 미 완성

---
+++ app/src/main/java/com/example/user/work2/MainActivity2.java
@@ -0,0 +1,13 @@
+package com.example.user.work2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.work2_2);
+    }
+}
