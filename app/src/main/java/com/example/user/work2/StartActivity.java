77f3d62588f38df8fb93b7c9e75a68f62e871f39
user188245 <kangsg10@naver.com>
2017-03-19 05:10:07 +0900

    레이아웃만 완성

--- app/src/main/java/com/example/user/work2/StartActivity.java
+++ app/src/main/java/com/example/user/work2/StartActivity.java
@@ -1,13 +1,39 @@
 package com.example.user.work2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+    Button b1,b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        init();
+    }
+
+    private void init(){
+        b1 = (Button)findViewById(R.id.button1);
+        b2 = (Button)findViewById(R.id.button2);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                if(v.getId() == R.id.button1) {
+                    intent = new Intent(StartActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+                else if(v.getId() == R.id.button2) {
+                    intent = new Intent(StartActivity.this, MainActivity2.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        b1.setOnClickListener(listener);
+        b2.setOnClickListener(listener);
     }
 }
